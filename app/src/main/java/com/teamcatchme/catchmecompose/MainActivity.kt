c451efc5076cbc29bf8a6b64ef6945c60876b761
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-07-31 23:53:09 +0900

    [FIX] :  Theme fix

--- app/src/main/java/com/teamcatchme/catchmecompose/MainActivity.kt
+++ app/src/main/java/com/teamcatchme/catchmecompose/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.teamcatchme.catchmecompose.ui.theme.CatchMeComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
